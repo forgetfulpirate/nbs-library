dc598ccbfb31f0d6671bd2a7e8227bbb4ee8c815
cayceevans213 <cevangelista2021@student.nbscollege.edu.ph>
2024-03-29 11:57:14 +0800

    edit

--- Source/librarian/notapprove.php
+++ Source/librarian/notapprove.php
@@ -3,6 +3,8 @@
 	$id= $_GET["id"];
 	mysqli_query($link, "update std_registration set status='no' where id=$id");
 	mysqli_query($link, "update t_registration set status='no' where id=$id");
+    mysqli_query($link, "update std_registration set verified='no' where id=$id");
+	mysqli_query($link, "update t_registration set verified='no' where id=$id");
  ?>
  <script type="text/javascript">
  	window.location="status.php";
