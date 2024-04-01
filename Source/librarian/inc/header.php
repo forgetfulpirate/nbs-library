f712d3eaa11131df83ab43c47228f40ba06987da
cayceevans213 <cevangelista2021@student.nbscollege.edu.ph>
2024-04-01 10:36:36 +0800

    edit

--- Source/librarian/inc/header.php
+++ Source/librarian/inc/header.php
@@ -305,7 +305,7 @@
 	<script src="inc/js/datatables.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
-<script src="https://cdn.datatables.net/v/dt/jszip-3.10.1/dt-2.0.3/b-3.0.1/b-html5-3.0.1/b-print-3.0.1/datatables.min.js"></script>
+    <script src="https://cdn.datatables.net/v/dt/jszip-3.10.1/dt-2.0.3/b-3.0.1/b-html5-3.0.1/b-print-3.0.1/datatables.min.js"></script>
 
     
 </body>
