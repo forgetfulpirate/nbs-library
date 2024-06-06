8cf6b8604e0801101deffe996048ed1f7ddb2e29
cayceevans213 <cevangelista2021@student.nbscollege.edu.ph>
2024-06-07 01:02:01 +0800

    123

--- Source/librarian/paid-fine.php
+++ Source/librarian/paid-fine.php
@@ -16,12 +16,12 @@
             echo "<script>window.location='fine.php';</script>";
         } elseif ($fine_data['status'] == 'no') {
             // Insert data into return_books
-            $insert_query = "INSERT INTO return_books (first_name, last_name, middle_name, student_number, utype, email, booksname, accession_number, date_issued, booksissuedate, booksreturndate) 
-                             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            $insert_query = "INSERT INTO return_books (first_name, last_name, middle_name, student_number, utype, email, booksname, accession_number, date_issued, booksissuedate, booksreturndate, issuedby) 
+                             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
 
             $stmt = mysqli_prepare($link, $insert_query);
-            mysqli_stmt_bind_param($stmt, "sssssssssss", $name, $last_name, $middle_name, $student_number, $utype, $email, $booksname, $accession_number, $date_issued, $booksissuedate, $booksreturndate);
+            mysqli_stmt_bind_param($stmt, "ssssssssssss", $name, $last_name, $middle_name, $student_number, $utype, $email, $booksname, $accession_number, $date_issued, $booksissuedate, $booksreturndate, $issuedby);
         
             $name = $fine_data['first_name'];
             $last_name = $fine_data['last_name'];
@@ -34,8 +34,32 @@
             $date_issued = $fine_data['date_issued'];
             $booksissuedate = $fine_data['booksissuedate'];
             $booksreturndate = $fine_data['booksreturndate'];
+            $issuedby = $fine_data['username'];
         
             mysqli_stmt_execute($stmt);
+        
+            // Insert data into return_history
+            $insert_history_query = "INSERT INTO return_books_archive (first_name, last_name, middle_name, student_number, utype, email, booksname, accession_number, date_issued, booksissuedate, booksreturndate, issuedby) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+            $stmt_history = mysqli_prepare($link, $insert_history_query);
+            mysqli_stmt_bind_param($stmt_history, "ssssssssssss", $name, $last_name, $middle_name, $student_number, $utype, $email, $booksname, $accession_number, $date_issued, $booksissuedate, $booksreturndate, $issuedby);
+
+            // Corrected variable binding
+            $name = $fine_data['first_name'];
+            $last_name = $fine_data['last_name'];
+            $middle_name = $fine_data['middle_name'];
+            $student_number = $fine_data['student_number'];
+            $utype = $fine_data['utype'];
+            $email = $fine_data['email'];
+            $booksname = $fine_data['booksname'];
+            $accession_number = $fine_data['accession_number'];
+            $date_issued = $fine_data['date_issued'];
+            $booksissuedate = $fine_data['booksissuedate'];
+            $booksreturndate = $fine_data['booksreturndate'];
+            $issuedby = $fine_data['username'];
+        
+            mysqli_stmt_execute($stmt_history);
         
             // Update status in finezone
             mysqli_query($link, "UPDATE finezone SET status='yes' WHERE id = $id");
--- Source/librarian/user/student/delete-return.php
+++ Source/librarian/user/student/delete-return.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    if (!isset($_SESSION["username"])) {
+    if (!isset($_SESSION["student"])) {
         ?>
         <script type="text/javascript">
             window.location="login.php";
